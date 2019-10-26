bf6e56c8d05b085e35aaa0fa7d705f6fd31a5f6c
Mjavala <mjabravo221@gmail.com>
2019-10-26 00:34:34 -0600

    first-commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    {{ info }}
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default({ // eslint-disable-line no-new
+  data () {
+    return {
+      info: null,
+      state: 'Y@&E#^IDB#YBDYD@(SN@'
+    }
+  },
+  mounted () {
+    axios({
+      method: 'get',
+      url: 'https://api.tradier.com/v1/markets/quotes',
+      headers: {
+        Authorization: 'Bearer q1MudAtYloMpux8HbD7PMt2pSG6l',
+        Accept: 'application/json'
+      },
+      params: {
+        symbols: 'AAPL',
+        greeks: 'false'
+      },
+      transformRequest: [(data, headers) => { delete headers.common.ContenType}]
+    })
+      .then(response => (this.info = response))
+  }
+})
+</script>
+
